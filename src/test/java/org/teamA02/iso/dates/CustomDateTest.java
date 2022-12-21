4ac8d60b5a7459383d5e4633e739beca70ca99f4
RaulMoya14 <115221547+RaulMoya14@users.noreply.github.com>
2022-12-21 22:10:19 +0100

    Final Version CustomDateTest Class
    All the methods are implemented and fix an unexpected error

--- src/test/java/org/teamA02/iso/dates/CustomDateTest.java
+++ src/test/java/org/teamA02/iso/dates/CustomDateTest.java
@@ -6,13 +6,17 @@ import org.junit.Test;
 
 public class CustomDateTest {
 
+	/**
+	 * Test method for {@link org.teamA02.iso.CustomDate#IsLeap()}.
+	 * For this test method we have used the test suite using each use
+	 */
 	@Test
 	public void TestIsLeap(){
 		
 		assertTrue(new CustomDate(15,5,1992).IsLeap());
 		assertTrue(new CustomDate(28,6,1600).IsLeap());
 		assertFalse(new CustomDate(29,2,1600).IsLeap());
-		assertFalse(new CustomDate(30,5,2021).IsLeap());
+		assertFalse(new CustomDate(29,1,2021).IsLeap());
 		
 		try{
 			assertTrue(new CustomDate(-98, -8, -1712).IsLeap());
@@ -24,8 +28,39 @@ public class CustomDateTest {
 		  } catch(IllegalArgumentException e) {}
 	}
 	
+	/**
+	 * Test method for {@link org.teamA02.iso.CustomDate#DayWithinMonth()}.
+	 *  * For this test method we have used some test cases from the test suite using pairwise
+	 */
 	@Test
 	public void TestDayWithinMonth() {
 		
+		assertTrue(new CustomDate(15,6,1992).DayWithinMonth());
+		assertTrue(new CustomDate(28,2,1600).DayWithinMonth());
+		try{
+			assertTrue(new CustomDate(29,-8,-1712).DayWithinMonth());
+			fail("Se esperaba excepcion IllegalArgument");
+		  } catch(IllegalArgumentException e) {}
+		try{
+			assertTrue(new CustomDate(-98,-8,2021).DayWithinMonth());
+			fail("Se esperaba excepcion IllegalArgument");
+		  } catch(IllegalArgumentException e) {}
+		try{
+			assertTrue(new CustomDate(200,2,1992).DayWithinMonth());
+			fail("Se esperaba excepcion IllegalArgument");
+		  } catch(IllegalArgumentException e) {}
+		try{
+			assertTrue(new CustomDate(30,-8,300).DayWithinMonth());
+			fail("Se esperaba excepcion IllegalArgument");
+		  } catch(IllegalArgumentException e) {}
+		try{
+			assertTrue(new CustomDate(31,-8,1992).DayWithinMonth());
+			fail("Se esperaba excepcion IllegalArgument");
+		  } catch(IllegalArgumentException e) {}
+		assertFalse(new CustomDate(31,2,300).DayWithinMonth());
+		assertTrue(new CustomDate(30,6,2021).DayWithinMonth());
+		assertTrue(new CustomDate(30,5,1992).DayWithinMonth());
+		assertTrue(new CustomDate(28,6,300).DayWithinMonth());
+		assertTrue(new CustomDate(29,2,2021).DayWithinMonth());
 	}
 }
